dacb1bc8450d4983a9e0b5f76dc5685187805524
Zihan Jin <admin@zihanjin.com>
2026-05-28 03:25:22 +1000

    feat: public api

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,21 @@
+use crate::evaluator::{Env, Value};
+use std::{io::Read, rc::Rc};
+
+pub mod evaluator;
 pub mod parser;
+pub mod prelude;
+
+pub fn parse_and_run<R: Read>(r: R) -> Result<Rc<Value>, RispError> {
+    let sexp = parser::Parser::new(r).parse()?;
+    let form: Value = sexp.into();
+    Ok(evaluator::eval(Rc::new(form), &Env::new())?)
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum RispError {
+    #[error(transparent)]
+    ParseError(#[from] parser::ParseError),
+
+    #[error(transparent)]
+    RuntimeError(#[from] evaluator::EvaluatorError),
+}
