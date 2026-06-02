d2b54f07fd5ab08c1dffae7f3f12e69aadf19555
Zihan Jin <admin@zihanjin.com>
2026-06-03 09:40:59 +1000

    feat: simple main to read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-fn main() {
-    let n = true as i64;
-    println!("{}", n);
+use std::io::stdin;
+
+use rizz::RizzError;
+
+fn main() -> Result<(), RizzError> {
+    let res = rizz::parse_and_run(stdin())?;
+    println!("{}", res.0);
+    Ok(())
 }
